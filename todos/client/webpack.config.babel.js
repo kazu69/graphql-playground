8164fd46918c71a9aedc328d25613f053dcc8933
kazu69 <kazu69web@gmail.com>
2019-02-07 08:42:28 +0900

    add js build setting files

---
+++ todos/client/webpack.config.babel.js
@@ -0,0 +1,61 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+export default {
+  mode: 'development',
+  cache: true,
+  entry: {
+    app: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[chunkhash].[id].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.mjs'],
+    alias: { '~': path.resolve(__dirname, 'src') },
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: JSON.stringify('development'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  devServer: {
+    before: app => app.get('/favicon.ico', (_, res) => res.status(200).send()),
+    historyApiFallback: true,
+    contentBase: './dist',
+    hot: true,
+    publicPath: '/',
+    host: '0.0.0.0',
+    port: 9000,
+    proxy: { '/graphql': 'http://localhost:10001' },
+    clientLogLevel: 'info',
+  }
+};
